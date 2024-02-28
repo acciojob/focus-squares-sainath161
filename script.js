f0c51f43f786e6c12b985892a519d80661dcf623
Sainath <71361447+sainath161@users.noreply.github.com>
2024-02-28 09:25:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+	const squares = document.querySelectorAll(".square");
+
+	squares.forEach(square => {
+		square.addEventListener("mouseover", function () {
+			squares.forEach(otherSquare => {
+				if (otherSquare !== square) {
+					otherSquare.style.backgroundColor = "#6F4E37";
+				}
+			});
+		});
+		square.addEventListener("mouseout", function () {
+			squares.forEach(otherSquare => {
+				otherSquare.style.backgroundColor = "#E6E6FA";
+			});
+		});
+	});
+});
